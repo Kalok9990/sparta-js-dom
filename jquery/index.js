837c1fa1c0d6b601644e0ec3dab16650eac6ec04
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-11-02 15:31:35 +0000

    files added

---
+++ jquery/index.js
@@ -0,0 +1,49 @@
+$(function(event){
+  console.log("DOM is ready");
+
+  var listItems = ["Go out","Come back" , "Buy food" , "Eat food"];
+
+  addListItem(listItems);
+
+  //append items to list
+  function addListItem(value) {
+    $(value).each(function(index, item){
+      $("#list").append($("<li>" + item + "</li>"));
+      $("li").each(function(indexe, li){
+        if(indexe%2 === 0){
+          $(li).addClass("even");
+        }
+        $("#count").html($("li").length);
+      })
+    });
+  }
+});
+
+
+
+// $(function(event){
+//   console.log("DOM is ready");
+// 
+//   var listItems = ["Go out","Come back" , "Buy food" , "Eat food"];
+//
+//   addListItem(listItems);
+//
+//   //append items to list
+//   function addListItem(value) {
+//     $(value).each(function(index, item){
+//       $("#list").append($("<li>" + item + "</li>"));
+//     });
+//   }
+//
+//   //add even to evens
+//   $("li").each(function(index, li){
+//     if(index%2 === 0){
+//       $(li).addClass("even")
+//     }
+//   })
+//
+//   // write the class loop here
+//   $("li").each(function(index){
+//     $("#count").html(index + 1);
+//   })
+// });
